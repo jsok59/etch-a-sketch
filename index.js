c1ca42da33efd25037ba2b54c9e16004d489242b
John Sok <98001557+jsok59@users.noreply.github.com>
2023-10-26 18:24:22 -0400

    create working board and change size button

---
+++ index.js
@@ -0,0 +1,38 @@
+
+createBoard(16);
+let color = "black";
+
+function createBoard(size) {
+  let board = document.querySelector(".board");
+  let squares = document.querySelectorAll(".board >div");
+  squares.forEach((div) => div.remove());
+  for (let i = 0; i < size * size; i++) {
+    let square = document.createElement("div");
+    board.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+    board.style.gridTemplateRows = `repeat(${size}, 1fr)`;
+    square.addEventListener("mouseover", (event) => {
+      event.target.style.backgroundColor = color;
+    });
+    board.appendChild(square);
+  }
+  
+}
+
+function resetBoard() {
+  let board = document.querySelector(".board");
+  let squares = board.querySelectorAll("div");
+  squares.forEach((square) => {
+    square.style.backgroundColor = "white";
+  })
+}
+
+function changeSize(input) {
+  input = +input;
+  if (input < 2 || input > 100) {
+    alert("The input must be within 2 and 100");
+  } else {
+    
+    createBoard(input);
+  }
+}
+
